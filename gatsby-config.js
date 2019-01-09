0641c7f6672a1519be2b65f4d9f8aeb2f1cd5f54
Regina Galambos <gina.galambos@gmail.com>
2019-01-09 16:43:41 +0100

    add transformer plugin & display posts list on index page

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,9 +7,10 @@ module.exports = {
         {
             resolve: `gatsby-source-filesystem`,
             options: {
-                name: `src`,
-                path: `${__dirname}/src/`,
+                name: `content`,
+                path: `${__dirname}/content/`,
             },
         },
+        `gatsby-transformer-remark`,
     ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,45 @@
 import React from "react"
+import { graphql } from 'gatsby';
 
 import Layout from '../components/layout';
 
-export default () => (
-    <Layout>
-        <h1>Index Page</h1>
-    </Layout>
-)
+export default ({ data }) => {
+    console.log(data)
+    const posts = data.allMarkdownRemark.edges
+    return (
+        <Layout>
+            <div className="posts">
+                {posts.map(({ node }) => (
+                    <article className="post" key={node.id}>
+                        <h1 className="post-title">
+                            {node.frontmatter.title}
+                        </h1>
+
+                        <span className="post-date">{node.frontmatter.date}</span>
+
+                        {node.excerpt}
+                    </article>
+                ))}
+            </div>
+        </Layout>
+    );
+}
+
+export const indexQuery = graphql`
+    query {
+        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+            totalCount
+            edges {
+                node {
+                    id
+                    frontmatter {
+                        title
+                        date(formatString: "DD MMMM YYYY")
+                    }
+                    html
+                    excerpt
+                }
+            }
+        }
+    }
+`
